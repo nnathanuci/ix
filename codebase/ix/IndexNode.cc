b5653fc22b7d607b538170dd09d512df5b7c4338
Naveen Nathan <nnathan@uci.edu>
2011-05-11 03:41:16 -0700

    darkhipo code.

---
+++ codebase/ix/IndexNode.cc
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "IndexNode.h"
+
+using namespace std;
+
+
+std::ostream& operator<<(std::ostream& os, const Entry& entry)
+{
+    os << "<V: " << entry.getKey() << " P->" << entry.getPage() << "> ";
+    return os;
+}
+
+int compare_entrys (const void* a, const void* b)
+{
+	Entry A = *( (Entry*) a);
+	Entry B = *( (Entry*) b);
+	return ( (int) A.getKey() - (int) B.getKey() );
+}
+
+std::ostream& operator<<(std::ostream& os, const IndexNode& node)
+{
+	os << "[ L: " << node.left << " R: " << node.right << " F: " << node.free_offset << " T: " << node.type << " C: " << node.num_entries << " ]:::: ";
+    for (unsigned int iter = 0; iter < node.num_entries ; iter++)
+    	os << node.entries[iter];
+    os << std::endl;
+    return os;
+}
+
+
+int IndexNode::insert(unsigned int key, unsigned int r_ptr)
+{
+	if ( type == TYPE_DATA )
+	{
+		/* insert into the node internally */
+		entries[num_entries].setKey(key);
+		entries[num_entries].setPage(r_ptr);
+		qsort (entries, num_entries, sizeof(Entry), compare_entrys);
+		num_entries++;
+		free_offset += sizeof(unsigned int);
+	}
+	else
+	{
+		perror("Only data pages currently supported.");
+	}
+
+	return 0;
+}
+
+int IndexNode::remove(unsigned int key)
+{
+	if ( type == TYPE_DATA )
+	{
+		Entry *found = (Entry*) bsearch ( &key, (entries), num_entries, sizeof(Entry), compare_entrys );
+
+		if ( found == NULL || (found == entries - 1) )
+			return -1;
+
+		entries[found - entries] = entries[num_entries - 1];
+		num_entries--;
+		free_offset -= sizeof(unsigned int);
+		qsort (entries, num_entries, sizeof(Entry), compare_entrys);
+	}
+	else
+	{
+
+	}
+	return 0;
+}
+
+void IndexNode::write_node(void* p)
+{
+	unsigned int* page = (unsigned int*) p;
+	unsigned int* page_trailer = ((unsigned int*) p ) + (ENTRY_TRAILER_START / sizeof(unsigned int)) ;
+
+    for (unsigned int iter = 0; iter <= num_entries ; iter++ , page+=2)
+    	entries[iter].write_entry(page);
+
+	*page_trailer = left;
+	page_trailer++;
+	*page_trailer = right;
+	page_trailer++;
+	*page_trailer = free_offset;
+	page_trailer++;
+	*page_trailer = type;
+	page_trailer++;
+	*page_trailer = num_entries;
+	page_trailer++;
+
+
+}
+
+void IndexNode::read_node(void* p)
+{
+	unsigned int* page = (unsigned int*) p;
+	unsigned int* page_trailer = ((unsigned int*) p ) + (ENTRY_TRAILER_START / sizeof(unsigned int)) ;
+
+    left = *page_trailer;
+    page_trailer++;
+	right = *page_trailer;
+	page_trailer++;
+	free_offset = *page_trailer ;
+	page_trailer++;
+	type = *page_trailer;
+	page_trailer++;
+	num_entries = *page_trailer ;
+	page_trailer++;
+
+    for (unsigned int iter = 0; iter <= num_entries ; iter++ , page+=2)
+    	entries[iter].read_entry(page);
+}
+
+int test_insert_delete_visual_1() {
+	IndexNode node;
+	node.insert(70,11);
+	cout << node ;
+	node.insert(21,12);
+	cout << node ;
+	node.insert(22,22);
+	cout << node ;
+	node.insert(35,33);
+	cout << node ;
+	node.insert(17,19);
+	cout << node ;
+	node.insert(23,11);
+	cout << node ;
+	node.remove(21);
+	cout << node ;
+	node.remove(35);
+	cout << node ;
+	node.remove(17);
+	cout << node ;
+	node.remove(23);
+	cout << node ;
+	node.remove(70);
+	cout << node ;
+	node.remove(22);
+	cout << node ;
+
+	return 0;
+}
+
+int test_read_write_to_bytes()
+{
+	IndexNode node, node_test;
+	node.insert(70,11);
+	node.insert(21,12);
+	node.insert(22,22);
+	node.insert(35,33);
+	node.insert(17,19);
+	node.insert(23,11);
+
+	void* p = calloc(MAX_ENTRIES,sizeof(Entry));
+	node.write_node(p);
+	node_test.read_node(p);
+	cout << node ;
+	cout << node_test ;
+
+	free(p);
+	return 0;
+}
+/*
+int main()
+{
+	test_read_write_to_bytes();
+}
+*/
---
+++ codebase/ix/IndexNode.h
@@ -0,0 +1,83 @@
+/*
+ * IndexNode.h
+ *
+ *  Created on: May 11, 2011
+ *      Author: DA
+ */
+#ifndef INDEXNODE_H_
+#define INDEXNODE_H_
+
+#include <string.h>
+
+#define ENTRY_TRAILER_START	(PF_PAGE_SIZE - 20)
+#define MAX_ENTRIES 		((PF_PAGE_SIZE - 20)/4)
+#define TYPE_DATA			(0)
+#define TYPE_INDEX			(1)
+#define TYPE_DELETED 		(2)
+#define NODE_HEADER_BYTES	(16)
+
+extern int compare_entrys (const void* a, const void* b);
+
+class Entry{
+	unsigned int key;
+	unsigned int r_page;
+public:
+    unsigned int getKey()  const		{return key		;}
+    unsigned int getPage() const		{return r_page  ;}
+    void setKey (unsigned int key   )	{this->key = key;}
+    void setPage(unsigned int r_page) 	{this->r_page = r_page;}
+    void write_entry(void* p)			{((unsigned int*) p)[0] = key;((unsigned int*) p)[1] = r_page;}
+    void read_entry (void* p)			{key = ((int*) p)[0] ; r_page = ((int*) p)[1] ;}
+
+    friend std::ostream& operator<<(std::ostream& os, const Entry& entry);
+};
+
+//http://www.java2s.com/Tutorial/Cpp/0180__Class/Usetypeidtotesttypeequality.htm }-> test class type.
+class IndexNode {
+	//1. left pid (0 means there is no left pid)
+	unsigned int  left;
+	//2. right pid (0 means there is no right pid
+	unsigned int  right;
+	//3. free space offset (initially 0) | used to calculate the amount of free space
+	unsigned int  free_offset;
+	//4. node type| 0: deleted node | 1: index node | 2: leaf node
+	unsigned int  type;
+	//5. number of items
+	unsigned int  num_entries;
+	//Account for trailer.
+	Entry  entries [MAX_ENTRIES] ; /******/
+
+public:
+	IndexNode ()
+	{
+		free_offset		= NODE_HEADER_BYTES;
+		left			= 0;
+		right			= 0;
+		num_entries		= 0;
+		type			= TYPE_DATA;
+
+		memset( entries, 0, sizeof(entries) );
+		entries[0].setKey (0);
+		entries[0].setPage(0);
+	} //offset is past the leftmost ptr.
+	~IndexNode() {}
+
+	int insert(unsigned int value, unsigned int r_ptr);
+	int remove(unsigned int value);
+	void write_node(void* p);
+	void read_node(void* p);
+
+    unsigned int getFree_offset() const				{return free_offset;};
+    unsigned int getLeft() const					{return left;};
+    unsigned int getRight() const					{return right;};
+    unsigned int getType() const					{return type;};
+    void setFree_offset(unsigned int free_offset)	{this->free_offset = free_offset;};
+    void setLeft(unsigned int left) 				{this->left = left;};
+    void setRight(unsigned int right)				{this->right = right;};
+    void setType(unsigned int type)					{this->type = type;};
+
+
+	friend std::ostream& operator<<(std::ostream& os, const IndexNode& node);
+};
+
+#endif /* INDEXNODE_H_ */
