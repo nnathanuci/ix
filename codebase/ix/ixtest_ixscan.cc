2cf7458c2e9143f15be9ec787d2244cb2e20792c
Naveen Nathan <nnathan@uci.edu>
2011-05-11 06:19:17 -0700

    Working on creating a mock data scan test.

--- codebase/ix/ixtest_ixscan.cc
+++ codebase/ix/ixtest_ixscan.cc
@@ -11,6 +11,12 @@
 
 using namespace std;
 
+/* temp dummy. */
+RC IX_IndexHandle::InsertEntry(void *data, RID const &rid)
+{
+    return 0;
+}
+
 void cleanup() // {{{
 {
   const char *files[4] = { "systemcatalog", "t1.a1", "t1.a2", "t1" };
@@ -102,6 +108,83 @@ void ixTest_Insert(IX_Manager *ixmgr)
     // }}}
 }
 
+void ixTest_data_test1(IX_Manager *ixmgr)
+{
+    RM *rm = RM::Instance();
+    IX_IndexHandle handle;
+    IX_IndexScan scan;
+
+    string data_test1 = "data_test1";
+    vector<Attribute> data_test1_attrs;
+    data_test1_attrs.push_back((struct Attribute) { "a1", TypeInt, 0 });
+    data_test1_attrs.push_back((struct Attribute) { "a2", TypeInt, 0 });
+
+
+    int key = 0;
+    RID rid = {0,0};
+    RID aux_rid;
+
+    /* create table, create index, insert an entry, scan/retrieve entry, delete index, delete table. */ // {{{
+    cout << "\n[ data_test1 ]" << endl;
+    ZERO_ASSERT(rm->createTable(data_test1, data_test1_attrs));
+    cout << "PASS: createTable(" << output_schema(data_test1, data_test1_attrs) << ")" << endl;
+
+    ZERO_ASSERT(ixmgr->CreateIndex(data_test1, "a1"));
+    cout << "PASS: CreateIndex(t1,a1)" << endl;
+
+    ZERO_ASSERT(ixmgr->OpenIndex(data_test1, "a1", handle));
+    cout << "PASS: OpenIndex(t1,a1,h)" << endl;
+
+    assert(handle.GetNumberOfPages() == 1);
+    cout << "PASS: h.GetNumberOfPages == 1 [data_test1.a1 has root node]" << endl;
+
+    {
+        /* create a data node (339 entries): [ (0,0,0) (10,100,1000) (20,200,2000) ... (3380, 33800, 338000) ] */
+        char data_buf[PF_PAGE_SIZE];
+        unsigned int data_pid;
+        unsigned int max_entries = (PF_PAGE_SIZE-20)/12; // (key, pageid, slotid)
+        unsigned int offset = 0;
+
+        for(int i=0; i<(int)max_entries; i++, offset += 12)
+        {
+            (*((int *) &data_buf[i*12])) = i*10;
+            (*((unsigned int *) &data_buf[i*12+4])) = i*100;
+            (*((unsigned int *) &data_buf[i*12+8])) = i*1000;
+        }
+
+        *((unsigned int *) &data_buf[PF_PAGE_SIZE-20]) = 0;
+        *((unsigned int *) &data_buf[PF_PAGE_SIZE-16]) = 0;
+        *((unsigned int *) &data_buf[PF_PAGE_SIZE-12]) = offset;
+        *((unsigned int *) &data_buf[PF_PAGE_SIZE-8]) = 2; // type data
+        *((unsigned int *) &data_buf[PF_PAGE_SIZE-4]) = max_entries; // num of entries
+    
+        ZERO_ASSERT(handle.NewNode(data_buf, data_pid));
+        assert(data_pid == 0);
+        cout << "PASS: handle.NewNode([data]) && data_pid == 0" << endl;
+        handle.DumpNode(0, 0);
+    }
+
+    //ZERO_ASSERT(scan.OpenScan(handle, EQ_OP, &key));
+    //cout << "PASS: scan.OpenScan(h,=,1)" << endl;
+
+    //ZERO_ASSERT(scan.GetNextEntry(aux_rid));
+    //assert(aux_rid.pageNum == 200 && aux_rid.slotNum == 3000);
+    //cout << "PASS: scan.GetNextEntry(aux_rid) && aux_rid == {200,3000}" << endl;
+    //
+    //assert(scan.GetNextEntry(aux_rid) == IX_EOF);
+    //cout << "PASS: scan.GetNextEntry(aux_rid) == IX_EOF" << endl;
+
+    //ZERO_ASSERT(scan.CloseScan());
+    //cout << "PASS: scan.CloseScan()" << endl;
+
+    ZERO_ASSERT(ixmgr->DestroyIndex(data_test1, "a1"));
+    cout << "PASS: DestroyIndex(data_test1,a1)" << endl;
+
+    ZERO_ASSERT(rm->deleteTable(data_test1));
+    cout << "PASS: deleteTable(" << data_test1 << ")" << endl;
+    // }}}
+}
+
 int main() 
 {
   cout << "test..." << endl;
@@ -110,7 +193,8 @@ int main()
   cleanup();
 
   cout << "Insert and IndexScan Tests" << endl << endl;
-  ixTest_Insert(ixmgr);
+  //ixTest_Insert(ixmgr);
+  ixTest_data_test1(ixmgr);
 
   cout << "OK" << endl;
 }
