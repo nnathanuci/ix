154fb58a472f1f9285e84beda6602a55a0c7a9f1
Naveen Nathan <nnathan@uci.edu>
2011-05-11 05:34:36 -0700

    Commented out darkhipo code, doesn't compile.

--- codebase/ix/ix.cc
+++ codebase/ix/ix.cc
@@ -1,31 +1,33 @@
 #include "ix.h"
-#include "IndexNode.h"
 
-extern int test_read_write_to_bytes();
+// none of this compiles.
+//#include "IndexNode.h"
 
-RC IX_IndexHandle::InsertEntry(void *key, const RID &rid)
-{
-	unsigned char* buffer [PF_PAGE_SIZE] = {0};
-	IndexNode node;
+//extern int test_read_write_to_bytes();
 
-	ReadNode(0,buffer);
-	/*Assuming that key is an int for now.*/
-	node.read_node(buffer);
-	node.insert(*((int*)key),rid.pageNum);
-	node.write_node(buffer);
-	WriteNode(0, buffer);
-	return 0;
-}
-
-RC IX_IndexHandle::DeleteEntry(void *key, const RID &rid)
-{
-	unsigned char* buffer [PF_PAGE_SIZE];
-	IndexNode node;
-	ReadNode(0,buffer);
-	/*Assuming that key is an int for now.*/
-	node.read_node(buffer);
-	node.remove(*((int*)key));
-	node.write_node(buffer);
-	WriteNode(0, buffer);
-	return 0;
-}
+//RC IX_IndexHandle::InsertEntry(void *key, const RID &rid)
+//{
+//	unsigned char* buffer [PF_PAGE_SIZE] = {0};
+//	IndexNode node;
+//
+//	ReadNode(0,buffer);
+//	/*Assuming that key is an int for now.*/
+//	node.read_node(buffer);
+//	node.insert(*((int*)key),rid.pageNum);
+//	node.write_node(buffer);
+//	WriteNode(0, buffer);
+//	return 0;
+//}
+//
+//RC IX_IndexHandle::DeleteEntry(void *key, const RID &rid)
+//{
+//	unsigned char* buffer [PF_PAGE_SIZE];
+//	IndexNode node;
+//	ReadNode(0,buffer);
+//	/*Assuming that key is an int for now.*/
+//	node.read_node(buffer);
+//	node.remove(*((int*)key));
+//	node.write_node(buffer);
+//	WriteNode(0, buffer);
+//	return 0;
+//}
