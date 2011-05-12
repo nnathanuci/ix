11a8c46a4b1ee14a19de002e65dd40a8d4cbcd81
Naveen Nathan <nnathan@uci.edu>
2011-05-12 07:30:30 -0700

    Equality tests done.

--- codebase/ix/ixtest_ixscan.cc
+++ codebase/ix/ixtest_ixscan.cc
@@ -120,7 +120,7 @@ void ixTest_data_test1(IX_Manager *ixmgr) // {{{
     data_test1_attrs.push_back((struct Attribute) { "a2", TypeInt, 0 });
 
 
-    /* single key equality test. */ // {{{
+    /* single key equality test [k1 k2 ... k* ... kn]. */ // {{{
     cout << "\n[ data_test1 - single key equality test ]" << endl;
     ZERO_ASSERT(rm->createTable(data_test1, data_test1_attrs));
     cout << "PASS: createTable(" << output_schema(data_test1, data_test1_attrs) << ")" << endl;
@@ -198,7 +198,7 @@ void ixTest_data_test1(IX_Manager *ixmgr) // {{{
     cout << "PASS: deleteTable(" << data_test1 << ")" << endl;
     // }}}
 
-    /* single key equality test (missing key). */ // {{{
+    /* single key equality test (missing key) [ki ki+1 ... kn], key: kj, s.t. j>n or j<i. */ // {{{
     cout << "\n[ data_test1 - single key equality test (missing key) ]" << endl;
     ZERO_ASSERT(rm->createTable(data_test1, data_test1_attrs));
     cout << "PASS: createTable(" << output_schema(data_test1, data_test1_attrs) << ")" << endl;
@@ -288,9 +288,8 @@ void ixTest_data_test1(IX_Manager *ixmgr) // {{{
     cout << "PASS: deleteTable(" << data_test1 << ")" << endl;
     // }}}
 
-
-    /* duplicate key equality test. */ // {{{
-    cout << "\n[ data_test1 - duplicate key equality test ]" << endl;
+    /* single key equality test [k1 k2 ... k* ... kn], key: kn */ // {{{
+    cout << "\n[ data_test1 - single key equality test (last key) ]" << endl;
     ZERO_ASSERT(rm->createTable(data_test1, data_test1_attrs));
     cout << "PASS: createTable(" << output_schema(data_test1, data_test1_attrs) << ")" << endl;
 
@@ -339,7 +338,7 @@ void ixTest_data_test1(IX_Manager *ixmgr) // {{{
     }
 
     {
-        int key = 90;
+        int key = 3380;
         RID aux_rid = {0, 0};
 
         /* equality test */
@@ -366,6 +365,119 @@ void ixTest_data_test1(IX_Manager *ixmgr) // {{{
     ZERO_ASSERT(rm->deleteTable(data_test1));
     cout << "PASS: deleteTable(" << data_test1 << ")" << endl;
     // }}}
+
+    /* duplicate key equality test. [k1 k1 k2 k2 ... kn] key: k1, k8 */ // {{{
+    cout << "\n[ data_test1 - duplicate key equality test ]" << endl;
+    ZERO_ASSERT(rm->createTable(data_test1, data_test1_attrs));
+    cout << "PASS: createTable(" << output_schema(data_test1, data_test1_attrs) << ")" << endl;
+
+    ZERO_ASSERT(ixmgr->CreateIndex(data_test1, "a1"));
+    cout << "PASS: CreateIndex(t1,a1)" << endl;
+
+    ZERO_ASSERT(ixmgr->OpenIndex(data_test1, "a1", handle));
+    cout << "PASS: OpenIndex(t1,a1,h)" << endl;
+
+    assert(handle.GetNumberOfPages() == 1);
+    cout << "PASS: h.GetNumberOfPages == 1 [root node]" << endl;
+
+    {
+        /* make the root node an occupied index node. */
+        unsigned int new_pid = 100;
+        char new_buf[PF_PAGE_SIZE] = {0};
+        unsigned int type = DUMP_TYPE_INDEX; memcpy((new_buf+PF_PAGE_SIZE-8), &type, sizeof(type));
+        ZERO_ASSERT(handle.NewNode(new_buf, new_pid));
+        assert(new_pid == 0);
+        cout << "PASS: h1.NewNode(new_buf[type=1]) && new_pid == 0 [overwrite root node as occupied index node]" << endl;
+    }
+
+    {
+        /* create a data node (339 entries): [ (0,0,0) (0,1,1) (10,100,1000) (10,101,1001)... (3380, 33801, 338001) ] */
+        char new_buf[PF_PAGE_SIZE];
+        unsigned int new_pid;
+        unsigned int max_entries = (PF_PAGE_SIZE-20)/12; // (key, pageid, slotid)
+        unsigned int offset = 0;
+
+        for(int i=0; i<(int)max_entries; i+=2)
+        {
+            (*((int *) &new_buf[i*12])) = i*10;
+            (*((unsigned int *) &new_buf[i*12+4])) = i*100;
+            (*((unsigned int *) &new_buf[i*12+8])) = i*1000;
+
+            /* create a second copy of the key [note the post increment is +2]. */
+            if ((i+1) < (int) max_entries)
+            {
+                (*((int *) &new_buf[(i+1)*12])) = i*10;
+                (*((unsigned int *) &new_buf[(i+1)*12+4])) = i*100 + 1;
+                (*((unsigned int *) &new_buf[(i+1)*12+8])) = i*1000 + 1;
+            }
+        }
+
+        offset = max_entries*12;
+        *((unsigned int *) &new_buf[PF_PAGE_SIZE-20]) = 0;
+        *((unsigned int *) &new_buf[PF_PAGE_SIZE-16]) = 0;
+        *((unsigned int *) &new_buf[PF_PAGE_SIZE-12]) = offset;
+        *((unsigned int *) &new_buf[PF_PAGE_SIZE-8]) = DUMP_TYPE_DATA; // type data
+        *((unsigned int *) &new_buf[PF_PAGE_SIZE-4]) = max_entries; // num of entries
+    
+        ZERO_ASSERT(handle.NewNode(new_buf, new_pid));
+        assert(new_pid == 1);
+        cout << "PASS: handle.NewNode([data]) && new_pid == 1" << endl;
+    }
+
+    {
+        // test two duplicate entries
+
+        int key = 0;
+        RID aux_rid = {0, 0};
+
+        ZERO_ASSERT(scan.OpenScan(handle, EQ_OP, &key, 1));
+        cout << "PASS: scan.OpenScan(h,=," << key << ")" << endl;
+
+        ZERO_ASSERT(scan.GetNextEntry(aux_rid));
+        assert((aux_rid.pageNum == (unsigned int) key*10) && (aux_rid.slotNum == (unsigned int) key*100));
+        cout << "PASS: scan.GetNextEntry(aux_rid) && aux_rid == {" << key*100 << "," << key*1000 << "}" << endl;
+
+        ZERO_ASSERT(scan.GetNextEntry(aux_rid));
+        assert((aux_rid.pageNum == (unsigned int) (key*10+1)) && (aux_rid.slotNum == (unsigned int) (key*100+1)));
+        cout << "PASS: scan.GetNextEntry(aux_rid) && aux_rid == {" << key*100 << "," << key*1000 << "}" << endl;
+        
+        assert(scan.GetNextEntry(aux_rid) == IX_EOF);
+        cout << "PASS: scan.GetNextEntry(aux_rid) == IX_EOF" << endl;
+
+        ZERO_ASSERT(scan.CloseScan());
+        cout << "PASS: scan.CloseScan()" << endl;
+
+        key = 80;
+
+        ZERO_ASSERT(scan.OpenScan(handle, EQ_OP, &key, 1));
+        cout << "PASS: scan.OpenScan(h,=," << key << ")" << endl;
+
+        ZERO_ASSERT(scan.GetNextEntry(aux_rid));
+        assert((aux_rid.pageNum == (unsigned int) key*10) && (aux_rid.slotNum == (unsigned int) key*100));
+        cout << "PASS: scan.GetNextEntry(aux_rid) && aux_rid == {" << key*100 << "," << key*1000 << "}" << endl;
+
+        ZERO_ASSERT(scan.GetNextEntry(aux_rid));
+        assert((aux_rid.pageNum == (unsigned int) (key*10+1)) && (aux_rid.slotNum == (unsigned int) (key*100+1)));
+        cout << "PASS: scan.GetNextEntry(aux_rid) && aux_rid == {" << key*100+1 << "," << key*1000+1 << "}" << endl;
+        
+        assert(scan.GetNextEntry(aux_rid) == IX_EOF);
+        cout << "PASS: scan.GetNextEntry(aux_rid) == IX_EOF" << endl;
+
+        ZERO_ASSERT(scan.CloseScan());
+        cout << "PASS: scan.CloseScan()" << endl;
+
+    }
+
+    ZERO_ASSERT(ixmgr->CloseIndex(handle));
+    cout << "PASS: CloseIndex(handle)" << endl;
+
+    ZERO_ASSERT(ixmgr->DestroyIndex(data_test1, "a1"));
+    cout << "PASS: DestroyIndex(data_test1,a1)" << endl;
+
+    ZERO_ASSERT(rm->deleteTable(data_test1));
+    cout << "PASS: deleteTable(" << data_test1 << ")" << endl;
+    // }}}
+
 
 } // }}}
 
